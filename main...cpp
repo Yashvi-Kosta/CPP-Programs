270cd2b7d4883eaf6ba686360b0fcba532c7c17a
Yashvi Kosta <yashvikosta41@gmail.com>
2026-07-28 13:18:20 +0530

    Updated code

---
+++ main...cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    // variable define
+   //int age = 17;
+   // cout << "my age is: " << age << endl;
+
+   //decalartion
+   //int age;
+   //cout << age;
+
+   //definition
+   int age = 17;
+   cout << age << endl;
+
+    return 0; 
+}
