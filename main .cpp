f9adc5d490720e6f6f4016b6f9b8545c86d43032
Yashvi Kosta <yashvikosta41@gmail.com>
2026-07-29 14:32:28 +0530

    Added c++ code

---
+++ main .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main (){
+
+    //variable decalre
+    int marks
+    cout << "Enter your marks"<< endl;
+
+    //input le rahe hai yaha par 
+    cin >> marks;
+    cout << "your marks:" << marks << endl;
+    
+    return 0; 
+}
