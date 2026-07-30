759664e478b2d77dd48d222f24658ebe6702090c
Yashvi Kosta <yashvikosta41@gmail.com>
2026-07-30 20:55:27 +0530

    Added c++ code

--- file.cpp
+++ file.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout<<"yashvi";
+    cout<<"riya";
     cout<<"kosta";
     return 0; 
 }
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
-int main (){
-    cout<<"heloo";
-    return 0; 
+
+int main() {
+
+    // int budget;
+    // cout << "enter your budget: " << endl;
+
+    // //input
+    // cin >> budget;
+
+    // if (budget > 2000000) {
+    //     cout << " your can buy scorpio" <<endl;
+    // }
+    // else {
+    //     cout << " you cannot buy scorpio" <<endl;
+    //      }
+
+//     
+
+int marks = 100;
+if (marks>90) {
+    cout<<"A";
+}
+else if(marks > 80) {
+cout << "B";
+}
+else if (marks > 70) {
+    cout << "c";
+}
+else if (marks > 60) {
+cout << "D";
+}
+return 0;
 }
